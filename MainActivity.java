237e05a5d31c045e2a2dd115ad306c9e68579000
zockerjustin2103-ux <zockerjustin2103@gmail.com>
2026-04-16 15:22:00 +0200

    Create MainActivity.java for todo list app

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private TaskAdapter taskAdapter;
+    private ArrayList<Task> taskList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        taskList = new ArrayList<>();
+        taskAdapter = new TaskAdapter(taskList);
+        recyclerView.setAdapter(taskAdapter);
+
+        // Load tasks from database
+        // Add task functionality here
+    }
+
+    // Methods for adding, deleting, and marking tasks as complete
+}
